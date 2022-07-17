19793d1ea9556dcdf30ce148f04832943a14a066
Pierre-William Lessard <lessardpw@gmail.com>
2022-07-17 16:03:21 -0400

    Added javadocs to Post.java

--- phase1/src/entities/Post.java
+++ phase1/src/entities/Post.java
@@ -33,30 +33,66 @@ public class Post implements Serializable {
         id = UUID.randomUUID();
     }
 
+    /**
+     * returns the title of this post
+     *
+     * @return String of the title of this post
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * returns the content of this post
+     *
+     * @return String of the content of this post
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * returns the author of this post as a username
+     *
+     * @return String of the username of the Author of this post
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * returns the unique id of this post as a UUID object
+     *
+     * @return UUID object uniquely identifying this post
+     */
     public UUID getId() {
         return id;
     }
 
+    /**
+     * sets a new title this post
+     *
+     * @param title String of the title to be set for this post
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * replaces previous content of this post
+     *
+     * @param content String of the content to be set for this post
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * returns the initial time this post was authored as a
+     * LocalDateTime object
+     *
+     * @return LocalDateTime object signifying when this post was authored
+     */
     public LocalDateTime getTimePosted() {
         return timePosted;
     }
