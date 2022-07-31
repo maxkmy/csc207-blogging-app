13eaa690f2eb0eee0987f8f071e80db8b69bc94b
maxkmy <maxkoh03@gmail.com>
2022-07-31 13:40:01 -0400

    Create presenter to fill jinja templates with context

---
+++ phase2/src/presenters/JinjaPresenter.java
@@ -0,0 +1,28 @@
+package presenters;
+import com.hubspot.jinjava.Jinjava;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class JinjaPresenter {
+    Map<String, Object> context;
+    Jinjava jinja = new Jinjava();
+    String template;
+    // context should contain field -> List[Field], submitBtnName -> String
+    public JinjaPresenter(Map<String, Object> context, String templatePath) throws IOException {
+        this.context = context;
+        try {
+            template = Files.readString(Paths.get(templatePath));
+        } catch(IOException e) { // TODO: handlers should redirect users to some error status code
+            throw e;
+        }
+    }
+
+    public String present() {
+        String res = jinja.render(template, context);
+        System.out.println(res);
+        return res;
+    }
+}
