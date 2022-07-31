8384848462676a6a72cced3d5f98777089c2185e
maxkmy <maxkoh03@gmail.com>
2022-07-31 12:12:08 -0400

    Test use of jinja templates

--- phase2/src/handlers/SignUpHandler.java
+++ phase2/src/handlers/SignUpHandler.java
@@ -1,20 +1,33 @@
 package handlers;
 
+import com.hubspot.jinjava.Jinjava;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.HashMap;
+
 public class SignUpHandler implements HttpHandler {
-
-
     @Override
     public void handleRequest(HttpServerExchange exchange) {
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
-        String output = "<html><form action='signUp' " + "method='post'>" +
-                "<input type=\"text\" id=\"username\" name=\"username\"><br><br> " +
-                "<input type=\"text\" id=\"password\" name=\"password\"><br><br>" +
-                "<input type=\"submit\" value=\"Sign Up\">" +
-                "</form></html>";
-        exchange.getResponseSender().send(output);
+        Map<String, String> context = new HashMap<>();
+        context.put("submitBtnName", "sign up");
+
+        Jinjava jinja = new Jinjava();
+        String fileName = "src/templates/loginSignUpForm.jinja";
+        String template = "";
+        try {
+            template = Files.readString(Paths.get(fileName));
+        } catch(IOException e) { // TODO: redirect to appropriate status code
+            System.out.println(e.getMessage());
+        }
+
+        String htmlOutput = jinja.render(template, context);
+        exchange.getResponseSender().send(htmlOutput);
     }
 }
