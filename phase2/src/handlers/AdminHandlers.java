9ecc4f9a4be51ebfb1e9b70d57f93855918ded67
Pierre-William Lessard <lessardpw@gmail.com>
2022-08-03 11:26:37 -0400

    Added AdminHandlers with promote functionality

--- phase2/src/handlers/AdminHandlers.java
+++ phase2/src/handlers/AdminHandlers.java
@@ -1,4 +1,23 @@
 package handlers;
 
+import controllers.admin.AdminController;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+import useCases.ManagerData;
+
 public class AdminHandlers {
+
+    private ManagerData managerData;
+    private AdminController adminController;
+
+    public AdminHandlers(ManagerData managerData) {
+        this.managerData = managerData;
+        adminController = new AdminController(managerData);
+    }
+
+    public void promote(HttpServerExchange exchange) {
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
+        String userToPromote = exchange.getQueryParameters().get("username").getFirst();
+        adminController.promote(userToPromote);
+    }
 }
