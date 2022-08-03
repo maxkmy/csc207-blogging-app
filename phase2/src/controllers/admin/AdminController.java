a90588f3b6b50ebbd5db0e263b303b3c9111087a
Pierre-William Lessard <lessardpw@gmail.com>
2022-08-03 11:55:08 -0400

    Added more admin methods to AdminController

--- phase2/src/controllers/admin/AdminController.java
+++ phase2/src/controllers/admin/AdminController.java
@@ -4,15 +4,21 @@ import exception.UserFollowedException;
 import exception.UserIsAdminException;
 import exception.UsernameNotFoundException;
 import useCases.AccountManager;
+import useCases.CommentManager;
 import useCases.ManagerData;
+import useCases.PostManager;
 
 public class AdminController {
 
     private AccountManager accountManager;
+    private PostManager postManager;
+    private CommentManager commentManager;
     private ManagerData managerData;
 
     public AdminController(ManagerData managerData) {
         accountManager = managerData.getAccountManager();
+        postManager = managerData.getPostManager();
+        commentManager = managerData.getCommentManager();
         this.managerData = managerData;
     }
 
@@ -20,7 +26,32 @@ public class AdminController {
         try {
             accountManager.promoteToAdmin(user);
         } catch (UsernameNotFoundException | UserIsAdminException e) {
-            System.out.println(user);
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void ban(String user) {
+        try {
+            accountManager.ban(user);
+        } catch (UsernameNotFoundException | UserIsAdminException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void unban(String user) {
+        try {
+            accountManager.unban(user);
+        } catch (UsernameNotFoundException | UserIsAdminException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void deleteUser(String user) {
+        try {
+            accountManager.deleteUser(user);
+            postManager.deletePostsWrittenBy(user);
+            commentManager.deleteCommentsWrittenBy(user);
+        } catch (UsernameNotFoundException | UserIsAdminException e) {
             System.out.println(e.getMessage());
         }
     }
