5589c67b03f8990c6de5d837681c9f458779ba3f
Pierre-William Lessard <lessardpw@gmail.com>
2022-08-06 10:48:17 -0400

    Added javadocs for AccountHandlers

--- phase2/src/handlers/AccountHandlers.java
+++ phase2/src/handlers/AccountHandlers.java
@@ -16,6 +16,12 @@ public class AccountHandlers extends Handlers {
         this.managerData = managerData;
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles GET requests for searching username
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void searchUsernameResults(HttpServerExchange exchange) {
         int limit = 10;
         Map<String, Deque<String>> props = exchange.getQueryParameters();
@@ -29,6 +35,12 @@ public class AccountHandlers extends Handlers {
         present(exchange, viewModel.getContext(), templatePath);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles GET request for displaying search page
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void searchUsername(HttpServerExchange exchange) {
         ViewModel viewModel = new ViewModel();
         viewModel.put("submitBtnName", "search");
@@ -38,30 +50,61 @@ public class AccountHandlers extends Handlers {
         present(exchange, viewModel.getContext(), templatePath);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles DELETE request for self deletion of account
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void deleteSelf(HttpServerExchange exchange) {
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
         accountController.deleteSelf();
         new LandingHandlers(managerData).login(exchange);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles DELETE request for following user
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void follow(HttpServerExchange exchange) {
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
         String userToFollow = exchange.getQueryParameters().get("username").getFirst();
         accountController.follow(managerData.getCurrentUser(), userToFollow);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles GET request for logging user out, refers to login handler
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void logout(HttpServerExchange exchange) {
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
         accountController.logout();
         new LandingHandlers(managerData).login(exchange);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles DELETE request for unfollowing user
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void unfollow(HttpServerExchange exchange) {
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
         String userToFollow = exchange.getQueryParameters().get("username").getFirst();
         accountController.unfollow(managerData.getCurrentUser(), userToFollow);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles GET request for displaying list of followers for
+     * specified account
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void followers(HttpServerExchange exchange) {
         String templatePath = "src/templates/followers.jinja";
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
@@ -74,6 +117,13 @@ public class AccountHandlers extends Handlers {
         present(exchange, viewModel.getContext(), templatePath);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles GET request for page displaying list of accounts
+     * following specified account
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void following(HttpServerExchange exchange) {
         String templatePath = "src/templates/followers.jinja";
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
@@ -86,6 +136,12 @@ public class AccountHandlers extends Handlers {
         present(exchange, viewModel.getContext(), templatePath);
     }
 
+    /**
+     * Represents HttpHandler Class
+     * Handles GET request for displaying requesters login history
+     *
+     * @param exchange An HTTP server request/response exchange
+     */
     public void viewHistory(HttpServerExchange exchange) {
         String templatePath = "src/templates/history.jinja";
         ViewModel viewModel = new ViewModel();
